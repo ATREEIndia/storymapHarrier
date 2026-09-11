87d6b947dcaa62193584aac5438cb8194c221135
Sreekuttan V N <sreekuttan@atree.org>
2026-09-11 17:28:06 +0530

    text update

--- components/Constants.tsx
+++ components/Constants.tsx
@@ -12,11 +12,8 @@ export const scroll_encounter = [
                 />
 
                 <p className="text-gray-800 text-lg ">
-                    We found Gangai on the day of a fire. He was a young
-                    bird, perched on a post, in the grassland, calling out
-                    to the other circling harriers. He tripped into our
-                    mist net at the time when the fire set by the fireline
-                    workers went momentarily out of control.
+                    We found Gangai on the day of a fire. He was a young bird, perched on a post, in the grazing lands, calling out to the other circling harriers. He tripped into our mist net around the time when a fire set by the fireline workers went momentarily out of control.
+
                 </p>
             </div>
         ),
@@ -28,8 +25,9 @@ export const scroll_encounter = [
         content: (
             <div className="flex flex-col gap-5">
                 <p className="text-gray-800 text-lg">
-                    We rushed in and retrieved the bird within a few 100 feet of the blazing grassland. The rest of the birds in the roost were flying helter-skelter and ultimately settled down in a patch close by.<br></br>
+                    We were within a few 100 feet of the blaze. Around us, the rest of the birds scattered in every direction before settling down in another roost close by, a place where they gather to rest. <br /><br />
                     What an experience for a young harrier in the first year of his migration.
+
                 </p>
             </div>
         ),
@@ -41,7 +39,8 @@ export const scroll_encounter = [
         content: (
             <div className="flex flex-col gap-5">
                 <p className="text-gray-800 text-lg">
-                    Gangai was our second harrier of the season and the team was excited. We named him after the village Gangewadi where we found him.
+                    He was our second harrier of the season, and the team was excited. We named him Gangai, after the village Gangewadi where we found him.
+
                 </p>
             </div>
         ),
@@ -53,7 +52,7 @@ export const scroll_encounter = [
         content: (
             <div className="flex flex-col gap-5">
                 <p className="text-gray-800 text-lg">
-                    After measuring wingspan, tail etc,  we slipped a ring on his legs, hoping this ring will help us keep in touch with Gangai as he grows from a scruffy brown juvenile into adulthood.
+                    We measured his wingspan, his tail and more. We slipped a ring on his legs, hoping it will help us keep in touch with Gangai as he grows from a scruffy brown juvenile into adulthood.
                 </p>
             </div>
         ),
@@ -66,6 +65,9 @@ export const scroll_encounter = [
             <div className="flex flex-col gap-5">
                 <p className="text-gray-800 text-lg">
                     Of the 16 harrier species present globally, the Indian subcontinent is an important wintering range for six species travelling from their breeding sites in Central Asia. India has the largest roost for Montagu’s and Pallid Harriers.
+
+
+
                 </p>
             </div>
         ),
@@ -80,6 +82,7 @@ export const scroll_encounter = [
                     <p className='font-semibold'>How do we know this?</p><br />
                     By counting them year after year for over 11 years across the grasslands of Rajasthan, Gujarat, Andhra Pradesh, Tamil Nadu and Maharashtra.
 
+
                 </span>
             </div>
         ),
@@ -91,7 +94,10 @@ export const scroll_encounter = [
         content: (
             <div className="flex flex-col gap-5">
                 <span className="text-gray-800 text-lg">
-                    T Ganesh's interest in Harriers goes back to the early 90’s when he started counting harrier roosts. He studied one such roost for 7 years on the outskirts of Hyderabad. The study resumed in 2015, when he and a small team of researchers began tracking Montagu's harrier migration by geotagging them at 6 sites.
+                    T. Ganesh's interest in Harriers goes back to the early 1990s when he started counting harrier roosts. One such was a roost on the outskirts of Hyderabad, which he studied for 7 years. <br /><br />
+
+                    The study resumed in 2015, when Ganesh and his small team of researchers began tracking Montagu's harrier migration by fitting them with satellite tags at 6 sites.
+
 
                 </span>
             </div>
@@ -105,9 +111,9 @@ export const scroll_encounter = [
         content: (
             <div className="flex flex-col gap-5">
                 <span className="text-gray-800 text-lg">
-                    But they had never tagged birds before.
-                    Raymond Klaassen and Ben Koks from the Dutch Montague Harrier foundation came to help.<br /><br />The team thus began tracking Gangai over the next … years.
-
+                    None of them had tagged a bird before. <br /><br />
+                    Raymond Klaassen and Ben Koks from the Dutch Montague Harrier foundation came to help.<br /><br />
+                    And so began the long work of tracking Gangai.
 
 
                 </span>
@@ -265,8 +271,15 @@ export const finding_roost
             content: (
                 <div className="flex flex-col gap-5">
                     <span className="text-gray-800 text-lg">
-                        <b>Stopover home:</b> When we tracked them through their autumn migration, it told us how each one of them stopped at the Thar Desert before reaching their destination in India. The stopover home location of different individuals overlapped across the years, helping us establish the importance of Thar Desert for these birds. <br></br>
-                        <a
+                        <b>Stopover home:</b> As we traced each one of them through their autumn migration over the years, we realised how they stopped at the Thar Desert in India before reaching their destination. This helped us establish the importance of Thar Desert for these birds.
+                        <br/><br/>
+                        We found out they stopped over the desert not so much in pursuit of the migrating locusts but for the resident grasshoppers, which formed their main prey.
+                        <br></br>
+
+                      
+
+                    </span>
+                      <a
                             href="https://www.sciencedirect.com/science/article/abs/pii/S0140196324001332"
                             target="_blank"
                             rel="noopener noreferrer"
@@ -274,32 +287,18 @@ export const finding_roost
                         >
                             Read More
                         </a>
-
-                    </span>
                 </div>
             ),
             media_type: "image",
             media_src:
                 'https://atree-communication.s3.amazonaws.com/storymap-harrier/photo/stopover.jpg',
         },
+       
         {
             content: (
                 <div className="flex flex-col gap-5">
                     <p className="text-gray-800 text-lg">
-                        We also found out they stopped over the desert not so much in pursuit of the migrating locusts but for the resident grasshoppers that formed their main prey.
-
-                    </p>
-                </div>
-            ),
-            media_type: "image",
-            media_src:
-                'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
-        },
-        {
-            content: (
-                <div className="flex flex-col gap-5">
-                    <p className="text-gray-800 text-lg">
-                        <b>Detour migration:</b> Another revelation was that migrating birds do not always travel along the shortest possible routes between breeding and wintering sites.
+                        <b>Detour migration:</b> Another revelation was that migrating birds do not always take the shortest route between their breeding and wintering sites. Nine years of tracking showed us how the harriers took a detour of the Himalaya even if it meant more flying hours. 
 
 
                     </p>
@@ -318,47 +317,47 @@ export const finding_roost
             media_src:
                 'https://atree-communication.s3.amazonaws.com/storymap-harrier/photo/tracks.png',
         },
-        {
-            content: (
-                <div className="flex flex-col gap-5">
-                    <p className="text-gray-800 text-lg">
-                        Nine years of tracking showed us how detours may be common. For the harriers, skirting the Himalayas not only helped them avoid this formidable ecological barrier within the Central Asian Flyway, but also provided access to foraging habitats, favourable wind patterns and reduced predation risk.
+        // {
+        //     content: (
+        //         <div className="flex flex-col gap-5">
+        //             <p className="text-gray-800 text-lg">
+        //                 Nine years of tracking showed us how detours may be common. For the harriers, skirting the Himalayas not only helped them avoid this formidable ecological barrier within the Central Asian Flyway, but also provided access to foraging habitats, favourable wind patterns and reduced predation risk.
 
 
-                    </p>
-                </div>
-            ),
-            media_type: "image",
-            media_src:
-                'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
-        },
-        {
-            content: (
-                <div className="flex flex-col gap-5">
-                    <span className="text-gray-800 text-lg">
-                        <b>Same place, same time: </b>Tracking Gangai and its fellow harriers was fascinating. We saw how they took the nearly similar route year after year across the continent.
+        //             </p>
+        //         </div>
+        //     ),
+        //     media_type: "image",
+        //     media_src:
+        //         'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
+        // },
+        // {
+        //     content: (
+        //         <div className="flex flex-col gap-5">
+        //             <span className="text-gray-800 text-lg">
+        //                 <b>Same place, same time: </b>Tracking Gangai and its fellow harriers was fascinating. We saw how they took the nearly similar route year after year across the continent.
 
-                    </span>
-                </div>
-            ),
-            media_type: "image",
-            media_src:
-                'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
-        },
-        {
-            content: (
-                <div className="flex flex-col gap-5">
-                    <p className="text-gray-800 text-lg">
-                        They flew across the same fields, fallows, hills and mountains, sometimes across a short gulf near Khambat in Gujarat, to reach the same location in Maharashtra’s Solapur every single year. When they left and when they returned also coincided.
+        //             </span>
+        //         </div>
+        //     ),
+        //     media_type: "image",
+        //     media_src:
+        //         'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
+        // },
+        // {
+        //     content: (
+        //         <div className="flex flex-col gap-5">
+        //             <p className="text-gray-800 text-lg">
+        //                 They flew across the same fields, fallows, hills and mountains, sometimes across a short gulf near Khambat in Gujarat, to reach the same location in Maharashtra’s Solapur every single year. When they left and when they returned also coincided.
 
 
-                    </p>
-                </div>
-            ),
-            media_type: "image",
-            media_src:
-                'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
-        },
+        //             </p>
+        //         </div>
+        //     ),
+        //     media_type: "image",
+        //     media_src:
+        //         'https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/day_of_researcher/harrier_gliding.jpg',
+        // },
 
 
     ]
--- components/Flashback.tsx
+++ components/Flashback.tsx
@@ -7,27 +7,29 @@ const Flashback = () => {
 
     return (
         <Magazine_img_tsx
-        img_right={true}
-        img_src="https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/mother_harrier_with_chicks.png"
-        content={
-          <div>
-            <Title
-                topTitle=""
-                mainTitle="Flashback"
-            />
-             <p className="mt-5">
-                    Gangai was born in  the meadows near lake Balkash in Kazakhstan. For the first few weeks , his parents looked after him. They hunted for  voles and ferried them to the babies in the nest.<br/><br/>
+            img_right={true}
+            img_src="https://atree-communication.s3.ap-south-1.amazonaws.com/storymap-harrier/photo/mother_harrier_with_chicks.png"
+            content={
+                <div>
+                    <Title
+                        topTitle=""
+                        mainTitle="Flashback"
+                    />
+                    <p className="mt-5">
+                        Gangai was born in  the meadows near lake Balkash in Kazakhstan. For his first few weeks of life, his parents looked after him. They hunted for  voles and ferried them to the babies in the nest. <br /><br />
 
-                    It is not just a ferrying flight, but an astonishing pass that might leave the best footballers in awe as one adult passes it to the other in mid air and they share parenting duties.<br/><br/>
+                        It is not just a ferrying flight, but an astonishing pass from one adult to another in mid air that might leave the best footballers in awe. Between them, the parents share the duties of raising him. <br /><br />
 
-                    Soon the adults left and he learnt to feed himself . As the days became shorter by winter, an inherent migratory instinct kicked in and Gangai started on his first migratory flight- A distance of 5000km over 22 days,  from Kazakhstan to India.  He was just over five months old.<br/><br/>
+                        Soon the adults left on their migration and Gangai learnt to feed himself . By winter, as the days became shorter, an inherent migratory instinct kicked in and Gangai started on his first migratory flight. A distance of 5000 km over 22 days,  from Kazakhstan to India.  He was just over five months old.
 
-                    
 
-                </p>
 
-          </div>}
-      />
+
+
+                    </p>
+
+                </div>}
+        />
     )
 }
 
